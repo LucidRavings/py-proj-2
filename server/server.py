a0fcecc292b61379fba91ce54eb62911c17eacc7
LucidRavings <cybernerd82@yahoo.com>
2023-04-21 13:57:09 -0600

    Implemented templates folder with HTML and CSS files

--- server/server.py
+++ server/server.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route("/all_cupcakes")
+def all_cupcakes():
+    return render_template("all_cupcakes.html")
+
+@app.route("/single_cupcake")
+def single_cupcake():
+    return render_template("single_cupcake.html")
+
+@app.route("/order")
+def order():
+    return render_template("order.html")
 
 if __name__ == "__main__":
     app.env = "development"
